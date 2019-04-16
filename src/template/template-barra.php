679f6af5c944f12a53d4aa691c8234832e06e2f1
Farias Anderson (GS/LOW41-LA) <faa5lov@bosch.com>
2019-04-16 17:02:12 -0300

    mini fix  navbar

--- src/template/template-barra.php
+++ src/template/template-barra.php
@@ -69,9 +69,8 @@ if ($_SESSION["usrlog"]) {
 </head>
 
 <body>
-    <div >
-    <nav class="navbar navbar-expand-lg navbar-bosch bg-bosch fixed-top"  >
-        
+  
+    <nav class="navbar navbar-expand-lg navbar-bosch bg-bosch fixed-top float-left"  >
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
             <span class="navbar-toggler-icon"></span>
         </button>
